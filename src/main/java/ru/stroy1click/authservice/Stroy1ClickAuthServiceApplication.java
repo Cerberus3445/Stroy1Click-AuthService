d576fb582b96b1196d2ec453668c19cf6c301cac
Egor <egormalov.2007@gmail.com>
2025-08-15 19:44:12 +0300

    Initial commit

---
+++ src/main/java/ru/stroy1click/authservice/Stroy1ClickAuthServiceApplication.java
@@ -0,0 +1,13 @@
+package ru.stroy1click.authservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Stroy1ClickAuthServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Stroy1ClickAuthServiceApplication.class, args);
+    }
+
+}
